47870cf59b7af81ed69fca09aeeae4ae46ebf5f2
Kashyap <khpatel4991@gmail.com>
2018-06-10 15:31:52 -0400

    Add mnaifest with some static assets

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,26 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx)
+    	return { ...initialProps }
+  	}
 
-  render() {
-    return (
-      <html>
-        <Head>
-		  <link href="/static/color.css" rel="stylesheet" />
-        </Head>
-        <body className="custom_class">
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    )
-  }
+  	render() {
+    	return (
+			<html>
+				<Head>
+					<link rel="icon" type="image/png" href="/static/favicon-32x32.png" sizes="32x32" />
+					<link rel="icon" type="image/png" href="/static/favicon-16x16.png" sizes="16x16" />
+					<link rel="manifest" href="/static/manifest.webmanifest" />
+					<link rel="stylesheet" href="/static/color.css" />
+					<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono" />
+				</Head>
+				<body className="custom_class">
+					<Main />
+					<NextScript />
+				</body>
+			</html>
+    	);
+  	}
 }
