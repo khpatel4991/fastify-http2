be46ade52caef152a8522116be6d9cd64ce0feba
Kashyap <khpatel4991@gmail.com>
2018-06-10 05:46:06 -0400

    Add transitions and loader component

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import App, { Container } from 'next/app';
 import Link from 'next/link';
+import { PageTransition } from  "next-page-transitions";
+import Loader from '../src/components/Loader';
+
+const TIMEOUT = 400
 
 export default class MyApp extends App {
 	static async getInitialProps ({ Component, router, ctx }) {
@@ -12,7 +16,7 @@ export default class MyApp extends App {
 	}
 
 	render () {
-		const {Component, pageProps} = this.props
+		const { Component, pageProps } = this.props;
 		return (
 			<Container>
 				<nav>
@@ -34,7 +38,46 @@ export default class MyApp extends App {
 						</li>
 					</ul>
 				</nav>
-				<Component {...pageProps} />
+				<PageTransition
+					timeout={TIMEOUT}
+					classNames='page-transition'
+					loadingComponent={<Loader />}
+					loadingDelay={500}
+					loadingTimeout={{
+						enter: TIMEOUT,
+						exit: 0
+					}}
+					loadingClassNames='loading-indicator'
+				>
+					<Component {...pageProps} />
+				</PageTransition>
+				<style jsx global>{`
+					.page-transition-enter {
+						opacity: 0;
+						transform: translate3d(0, 20px, 0);
+					}
+					.page-transition-enter-active {
+						opacity: 1;
+						transform: translate3d(0, 0, 0);
+						transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
+					}
+					.page-transition-exit {
+						opacity: 1;
+					}
+					.page-transition-exit-active {
+						opacity: 0;
+						transition: opacity ${TIMEOUT}ms;
+					}
+					.loading-indicator-appear,
+					.loading-indicator-enter {
+						opacity: 0;
+					}
+					.loading-indicator-appear-active,
+					.loading-indicator-enter-active {
+						opacity: 1;
+						transition: opacity ${TIMEOUT}ms;
+					}
+				`}</style>
 			</Container>
 		);
 	}
